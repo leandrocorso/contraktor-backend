aa66367ca3e41869e9c336222cb6e45d359aff6f
Leandro Corso <contato@leandrocorso.com.br>
2019-08-04 13:25:25 -0300

    CORS enabled in PHP

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -27,6 +27,10 @@ $app->withFacades();
 
 $app->withEloquent();
 
+header("Access-Control-Allow-Headers: Authorization, Content-Type");
+header("Access-Control-Allow-Origin: *");
+header('content-type: application/json; charset=utf-8');
+
 /*
 |--------------------------------------------------------------------------
 | Register Container Bindings
